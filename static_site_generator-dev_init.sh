fb4d74c23eee366a77a4b159ccee63b99dc2d3a8
aswe323 <matanwx135@gmail.com>
2025-01-29 11:39:01 +0200

    dev shell rename

---
+++ static_site_generator-dev_init.sh
@@ -0,0 +1,5 @@
+go install github.com/bootdotdev/bootdev@latest
+export PATH="$PATH:/home/matan/go/bin/"
+python3 -m venv venv
+set VIRTUAL_ENV "/home/matan/bootdev/statis_site_generator"
+#nix-shell -p python312Packages.pygame
