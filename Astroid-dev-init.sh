e991bdfc5de614fd5c5c2b46838a2252737dbed5
aswe323 <matanwx135@gmail.com>
2025-01-29 11:40:39 +0200

    Astroid-dev-init.sh renamed to diff shell, removed form repo

--- Astroid-dev-init.sh
+++
@@ -1,5 +0,0 @@
-go install github.com/bootdotdev/bootdev@latest
-export PATH="$PATH:/home/matan/go/bin/"
-python3 -m venv venv
-set VIRTUAL_ENV "/home/matan/bootdev/statis_site_generator"
-#nix-shell -p python312Packages.pygame
